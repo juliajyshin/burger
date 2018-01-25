e9f60f56963182b53bcece529bf591732821b9f5
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2018-01-25 02:39:27 -0800

    initial site files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Cheese Burger', false);
