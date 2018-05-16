f8b459cd0b9a2aaa4bc05434d3f24af827c60c51
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2018-05-15 19:12:50 -0700

    add jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,25 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "juliashin",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "juliashin",
+    database: "burger_db"
+  });
+};
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "juliashin",
+//   database: "burger_db"
+// });
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
