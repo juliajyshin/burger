4b53fd7713838598db0997a89cfb486681860054
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2018-05-15 20:17:58 -0700

    config

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if(process.env.JAWSDB_URL) {
 } else {
   //local host
     connection = mysql.createConnection({
-        // root: 3000,
+        port: 3306,
         host: "localhost",
         user: "root",
         password: "juliashin",
@@ -14,24 +14,6 @@ if(process.env.JAWSDB_URL) {
     });
 };
 
-
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "juliashin",
-//     database: "burger_db"
-//   });
-// };
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "juliashin",
-//   database: "burger_db"
-// });
 
 
 connection.connect(function (err) {
