e1edcc1bf68e6d19eadb2c0df73131458af213e2
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2018-05-15 20:02:12 -0700

    heorku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverried = require("method-override");
-
+var mysql = require("mysql");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
